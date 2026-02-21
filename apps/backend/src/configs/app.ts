ef076df4399494d60f354fc3f3e224b54d698a6c
BADtochka <badtochka@gmail.com>
2026-02-21 11:08:46 +0300

    remove sensitive file

--- apps/backend/src/configs/app.ts
+++
@@ -1,9 +0,0 @@
-import { AppConfigSchema } from '@/constants/AppConfigSchema';
-import z from 'zod';
-
-export const APP_CONFIG: z.infer<typeof AppConfigSchema> = {
-  POSTGRES_DB: 'postgres',
-  POSTGRES_USER: 'dev',
-  POSTGRES_PASSWORD: 'dev',
-  FRONTEND_URL: 'http://localhost:5173',
-};
